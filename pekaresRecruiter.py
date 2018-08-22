dff2d5af62b15c4b36f69afd4c72c2f8237cb8c7
Hanzlu <41087554+Hanzlu@users.noreply.github.com>
2018-08-22 09:46:06 +0300

    Update 1.3
    Thanks to MilkyWay90 for proposal.
    Used a few of their changes.

--- pekaresRecruiter.py
+++ pekaresRecruiter.py
@@ -7,15 +7,12 @@
 #1.1.1 added what time a telegram was sent at, and minor changes
 #1.2 doesn't send telegram to same nation twice
 #1.2.1 made the code shorter
-
+#1.3 Thanks to MilkyWay90 for their proposal. Include keys and id through input and
+#tries to run the program, running it again after a minute if error was given
 
 #to use:
-#fill in your "client key", "tgid" and "secret key" below 
-
-client = ""
-tgid = ""
-secret = ""
-
+#run, and fill in your "client key", "tgid" and "secret key" when asked 
+#for errrors, read the read.me
 
 import time
 import urllib.request
@@ -25,29 +22,38 @@ x = 1
 class old:
     old = ""
 
+client = input("Enter client key: ")
+tgid = input("Enter telegram id: ")
+key = input("Enter secret key: ")
+
 
 while x > 0:
     
-    name = ""
+    try:
+        name = ""
     
-    #page with new nations
-    page = urllib.request.urlopen("https://www.nationstates.net/cgi-bin/api.cgi?q=newnations")
-    content = str(page.read())
-    index = content.index(",")
-    name = content[23:index]
-    
-    #if trying to send to same nation twice
-    while old.old == name:
-        time.sleep(30)
+        #page with new nations
         page = urllib.request.urlopen("https://www.nationstates.net/cgi-bin/api.cgi?q=newnations")
         content = str(page.read())
         index = content.index(",")
         name = content[23:index]
+    
+        #if trying to send to same nation twice
+        while old.old == name:
+            time.sleep(30)
+            page = urllib.request.urlopen("https://www.nationstates.net/cgi-bin/api.cgi?q=newnations")
+            content = str(page.read())
+            index = content.index(",")
+            name = content[23:index]
         
-    #send telegram
-    urllib.request.urlopen("https://www.nationstates.net/cgi-bin/api.cgi?a=sendTG&client=%s&to=%s&tgid=%s&key=%s" % (client, name, tgid, secret))
+        #send telegram
+        urllib.request.urlopen("https://www.nationstates.net/cgi-bin/api.cgi?a=sendTG&client=%s&to=%s&tgid=%s&key=%s" % (client, name, tgid, key))
         
-    print("telegram sent to" , name, "at", time.strftime("%H:%M:%S"))
-    old.old = name
+        print("telegram sent to" , name, "at", time.strftime("%H:%M:%S"))
+        old.old = name
+    
+    except:
+        time.sleep(60)
+        continue
     
     time.sleep(180)
